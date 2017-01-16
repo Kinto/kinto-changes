729d4689ac0678be2164bb4fe06259d5e79a3a15
Mathieu Leplatre <mathieu@mozilla.com>
2017-01-16 15:30:53 +0100

    Timestamp is greater or equal

--- tests/test_changes.py
+++ tests/test_changes.py
@@ -95,7 +95,7 @@ class UpdateChangesTest(BaseWebTest, unittest.TestCase):
         last_modified = resp.json['data']['last_modified']
         resp = self.app.get(self.changes_uri, headers=self.headers)
         change_last_modified = resp.json['data'][0]['last_modified']
-        self.assertGreater(change_last_modified, last_modified)
+        self.assertGreaterEqual(change_last_modified, last_modified)
 
     def test_record_with_old_timestamp_does_update_changes(self):
         resp = self.app.post_json(self.records_uri, SAMPLE_RECORD,
