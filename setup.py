7103888af490b72880f9ebabb41591a1fdd17b13
Rémy HUBSCHER <rhubscher@mozilla.com>
2017-11-16 11:38:00 +0100

    Back to development: 1.2.0

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ test_requirements = [
 
 setup(
     name='kinto-changes',
-    version='1.1.0',
+    version='1.2.0.dev0',
     description="Plug Kinto notifications to a collection endpoint.",
     long_description=readme + '\n\n' + history,
     author='Mozilla Services',
