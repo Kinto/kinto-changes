e69a39df0c49b97400ffaa3da7ff2a06ee442980
Ethan Glasser-Camp <ethan@betacantrips.com>
2018-08-21 14:24:07 -0400

    Add a test for initialization
    This should add coverage for the track_timestamp stuff that we do on
    startup.

--- tests/test_events.py
+++ tests/test_events.py
@@ -56,6 +56,45 @@ class RedirectEventsTest(BaseWebTest, unittest.TestCase):
                                   e.payload.get('collection_id') == 'changes']
         self.assertEqual(len(monitor_changes_events), 0)
 
+    def test_scan_timestamps_on_startup(self):
+        self.create_collection('blocklists', 'pinning')
+        res = self.app.get('/buckets/blocklists/collections/pinning/records', headers=self.headers)
+        last_modified = int(res.headers['ETag'][1:-1])
+
+        old_storage = self.storage
+        old_permission = self.permission
+
+        def restore_storage(config):
+            """Bring old app's storage/permission (memory-based) over to a new app.
+
+            This lets us test initialization in the case of existing collections."""
+            config.registry.storage = old_storage
+            config.registry.permission = old_permission
+
+        with mock.patch('tests.test_events', restore_storage):
+            self.app = self.make_app({
+                'kinto.includes': 'tests.test_events kinto_changes'
+            })
+
+        self.app.post_json('/buckets/blocklists/collections/pinning/records', SAMPLE_RECORD,
+                           headers=self.headers)
+        events = [call[0][0] for call in self.listener.call_args_list]
+        monitor_changes_events = [e for e in events
+                                  if e.payload.get('bucket_id') == 'monitor' and
+                                  e.payload.get('collection_id') == 'changes']
+        self.assertEqual(len(monitor_changes_events), 1)
+        event = monitor_changes_events[0]
+
+        # ID for buckets/blocklists/collections/pinning
+        actual_impacted_record_ids = ['c5b1aefc-bbcc-af1f-df42-73e219236f23']
+
+        self.assertEqual(
+            [r['new']['id'] for r in event.impacted_records],
+            actual_impacted_record_ids
+        )
+        self.assertEqual(event.impacted_records[0]['old']['last_modified'], last_modified)
+
+
 class StatsdTest(BaseWebTest, unittest.TestCase):
     def test_statsd_wraps_subscriber(self):
         config = mock.MagicMock()
