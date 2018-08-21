7b346eb8f80b75d4ffb6ef1d8f078c983c88f3c7
Ethan Glasser-Camp <ethan@betacantrips.com>
2018-08-21 11:05:26 -0400

    Add some tests

--- tests/test_events.py
+++ tests/test_events.py
@@ -2,6 +2,7 @@ import mock
 import unittest
 
 from . import BaseWebTest
+from kinto_changes import includeme
 
 SAMPLE_RECORD = {'data': {'dev-edition': True}}
 
@@ -25,6 +26,7 @@ class RedirectEventsTest(BaseWebTest, unittest.TestCase):
     def setUp(self):
         super(RedirectEventsTest, self).setUp()
         self.registry = self.app.app.registry
+        self.listener.call_args_list = []
 
     def test_generate_event(self):
         self.app.post_json(self.records_uri, SAMPLE_RECORD,
@@ -43,3 +45,25 @@ class RedirectEventsTest(BaseWebTest, unittest.TestCase):
             [r['new']['id'] for r in event.impacted_records],
             actual_impacted_record_ids
         )
+
+    def test_ignore_event_for_unmonitored(self):
+        self.create_collection('foo', 'certificates')
+        self.app.post_json('/buckets/foo/collections/certificates/records',
+                           SAMPLE_RECORD, headers=self.headers)
+        events = [call[0][0] for call in self.listener.call_args_list]
+        monitor_changes_events = [e for e in events
+                                  if e.payload.get('bucket_id') == 'monitor' and
+                                  e.payload.get('collection_id') == 'changes']
+        self.assertEqual(len(monitor_changes_events), 0)
+
+class StatsdTest(BaseWebTest, unittest.TestCase):
+    def test_statsd_wraps_subscriber(self):
+        config = mock.MagicMock()
+        config.settings = {}
+        statsd = config.registry.statsd
+        statsd_decorate = statsd.timer.return_value
+
+        includeme(config)
+        self.assertEqual(statsd.timer.call_args_list[0][0][0], 'plugins.kinto_changes')
+        # Check that we're monitoring some calls.
+        self.assertTrue(statsd_decorate.call_count >= 2)
