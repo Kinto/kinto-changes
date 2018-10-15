24bfc6d0d991cf35c81e4cd658890411a80938c9
Mathieu Leplatre <mathieu@mozilla.com>
2018-10-15 16:05:46 +0200

    Back to development: 1.3.1

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ test_requirements = [
 
 setup(
     name='kinto-changes',
-    version='1.3.0',
+    version='1.3.1.dev0',
     description="Plug Kinto notifications to a collection endpoint.",
     long_description=readme + '\n\n' + history,
     author='Mozilla Services',
