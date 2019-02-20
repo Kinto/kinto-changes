f4c7cd11df1c2e804a62dfd5f0904beef1c4a769
Mathieu Leplatre <mathieu@mozilla.com>
2019-02-20 18:25:19 +0100

    Preparing release 2.1.0

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ test_requirements = [
 
 setup(
     name='kinto-changes',
-    version='2.0.1.dev0',
+    version='2.1.0',
     description="Plug Kinto notifications to a collection endpoint.",
     long_description=readme + '\n\n' + history,
     author='Mozilla Services',
