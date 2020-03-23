a711f9398f1185bb2c5a2672c1c60793ef9f1d08
Mathieu Leplatre <mathieu@mozilla.com>
2020-03-23 18:07:32 +0100

    Make registry.command optional (fixes #49)

--- kinto_changes/__init__.py
+++ kinto_changes/__init__.py
@@ -28,7 +28,7 @@ def includeme(config):
     # This is helpful because we try to get a snapshot of the database
     # when the application is created. But if we're doing a migrate,
     # there may not be a database.
-    if getattr(config.registry, 'command') == 'migrate':
+    if getattr(config.registry, 'command', None) == 'migrate':
         return
 
     config.add_api_capability(
