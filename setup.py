4a1ae677ea811707f54df9ed289c7e32e19558f4
Mathieu Leplatre <mathieu@mozilla.com>
2021-11-15 16:14:37 +0100

    Preparing release 4.1.1

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ test_requirements = [
 
 setup(
     name='kinto-changes',
-    version='4.1.1.dev0',
+    version='4.1.1',
     description="Plug Kinto notifications to a collection endpoint.",
     long_description=readme + '\n\n' + history,
     author='Mozilla Services',
